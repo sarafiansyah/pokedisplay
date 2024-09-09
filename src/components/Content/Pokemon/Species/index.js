2d1f05a137de7370d17ab9c596671689fd8d98e1
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2024-09-09 16:55:02 +0700

    Resp Fix 2

--- src/components/Content/Pokemon/Species/index.js
+++ src/components/Content/Pokemon/Species/index.js
@@ -448,7 +448,7 @@ const ResponsiveCarousel = () => {
                       gap={2}
                       sx={{
                         "@media (max-width: 600px)": {
-                          marginLeft: -0,
+                          marginLeft: -1,
                         },
                       }}
                     >
